767d7cf6c267fe9ccf8477108d625bc0945e5d0a
Calamity Jane <calamity_jane@macbook-air-de-calamity.home>
2023-04-07 19:16:13 +0200

    day three

--- 2015/day_three.py
+++ 2015/day_three.py
@@ -1,34 +1,52 @@
 ### Day one - Advent of code 2015 ###
 ### By Calamity Jane ###
 
-def change_tuple_value(t : tuple, idx: int, val: int) -> tuple :
-    lst = list(t)
-    lst[idx] = lst[idx] + val
-    return tuple(lst)
+def create_itineraries(itinerary: list) -> tuple : 
+    idx = 0 
+    santa_it = []
+    robot_santa_it = []
+    for i in itinerary : 
+        if idx %2 == 0 :
+            santa_it.append(i)
+        else :
+            robot_santa_it.append(i)
+        idx += 1
+
+    return (santa_it, robot_santa_it)
+
+def save_coordinates(itinerary: list, coordinates_set: set) -> set :
+    coordinates = (0,0)
+    for direction in itinerary: 
+        match direction:
+            case '>':
+                coordinates = (coordinates[0]+1, coordinates[1]) 
+            case '<':
+                coordinates = (coordinates[0]-1, coordinates[1])
+            case '^':
+                coordinates = (coordinates[0], coordinates[1]+1) 
+            case 'v':
+                coordinates = (coordinates[0], coordinates[1]-1)
+
+        coordinates_set.add(coordinates)
+
+    return coordinates_set
 
 def main() : 
   
     with open("puzzle_inputs/day_three.txt", "r") as inputs:
         directions = list(inputs.read())
 
-    coordinates = (0,0)
-    coordinates_set = {coordinates}
+    itineraries = create_itineraries(directions)
+
+    # Adds better lisibility for the code
+    santa_itinerary = itineraries[0]
+    robot_itinerary = itineraries[1]
+
+    houses_visited_by_santa = save_coordinates(santa_itinerary, {(0,0)})
+    houses_visited_by_robot = save_coordinates(robot_itinerary, houses_visited_by_santa)
     
-    for direction in directions : 
-        match direction:
-            case '>':
-                coordinates = change_tuple_value(coordinates, 0, 1)
-            case '<':
-                coordinates = change_tuple_value(coordinates, 0, -1)
-            case '^':
-                coordinates = change_tuple_value(coordinates, 1, 1)
-            case 'v':
-                coordinates = change_tuple_value(coordinates, 1, -1)
-        
-        coordinates_set.add(coordinates)
-    
-    print(len(coordinates_set))
-   
+    print(len(houses_visited_by_robot) )
+
 
 if __name__ == "__main__":
     main()
